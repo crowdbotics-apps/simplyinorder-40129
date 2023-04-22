af8750cfa9469b854b1ef87595f9889293d6a608
Crowdbotics <team@crowdbotics.com>
2023-04-22 23:45:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "simplyinorder-40129",
+  url: "https://simplyinorder-40129.botics.co",
+  api: "https://simplyinorder-40129.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
